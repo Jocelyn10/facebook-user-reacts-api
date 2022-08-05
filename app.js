cad8e1b9c8f2436afc963fceede3327ee9cb7205
Jocelyn10 <jocelynnsa@gmail.com>
2022-08-05 17:04:22 +0100

    feat: prepare datas for deploy

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const puppeteer = require('puppeteer');
+
+const { FACEBOOK_EMAIL, FACEBOOK_PASSWORD } = require('./config');
 
 const app = express();
 
@@ -25,13 +28,72 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
+app.get('/', async ({ query }, res) => {
+  function delay(time) {
+    return new Promise(function (resolve) {
+      setTimeout(resolve, time);
+    });
+  }
 
-    console.log("Hello !");
+  const browser = await puppeteer.launch({ headless: true });
+  let response = [];
+
+  const page = await browser.newPage();
+  await page.goto('https://www.facebook.com/', { timeout: 0 });
+  console.log('🤖🤖🤖🤖🤖 WAITING FOR LOGIN INPUTS 🤖🤖🤖🤖🤖🤖');
+  await page.waitForSelector('input[name="email"]');
+
+  await page.type('input[name="email"]', FACEBOOK_EMAIL);
+  await page.type('input[name="pass"]', FACEBOOK_PASSWORD);
+  await page.keyboard.press('Enter');
+
+  console.log('🤖🤖🤖🤖🤖 LOGING 🤖🤖🤖🤖🤖🤖');
+
+  await page.waitForNavigation({ waitUntil: 'networkidle2' });
+
+  console.log('🤖🤖🤖🤖🤖 GOING TO THE POST 🤖🤖🤖🤖🤖🤖');
+
+  await page.goto(query.url, { waitUntil: 'networkidle2', timeout: 0 });
+
+  console.log('🤖🤖🤖🤖🤖 CLICK ON LIKE BTN 🤖🤖🤖🤖🤖🤖');
+
+  await page.waitForSelector('img[role="presentation"]');
+  await page.click('img[role="presentation"]');
+
+  page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+  await page.waitForSelector('div[aria-label="Fermer"]');
+
+  await delay(7000);
+
+  console.log('🤖🤖🤖🤖🤖 GETING DATA FROM MODAL 🤖🤖🤖🤖🤖');
+
+  const links = await page.$$eval('div[role="dialog"]', (divs) =>
+    divs.map((div) => {
+      return {
+        html: div.innerHTML,
+      };
+    })
+  );
+
+  const modal = links[1];
+
+  var patt = /<a[^>]*label=["']([^"']*)["']/g;
+
+  console.log('🤖🤖🤖🤖🤖 PERSONS WHO LIKE THE POST 🤖🤖🤖🤖🤖🤖');
+  console.log('                                             ');
+  console.log('                                             ');
+  console.log('                ---🤖---                     ');
+  while ((match = patt.exec(modal.html))) {
+    console.log(match[1]);
+    response.push(match[1]);
+  }
+  console.log('                ---🤖---                     ');
+
+  await browser.close();
+
   res.set('Content-Type', 'text/html');
-  res.status(200).send('<h1>Hello GFG Learner!</h1>');
-
-  
+  res.status(200).send(response);
 });
 
 module.exports = app;
---
+++ config.js
@@ -0,0 +1,4 @@
+const FACEBOOK_EMAIL = 'jo12.blog@gmail.com';
+const FACEBOOK_PASSWORD = 'ghfg2@4654GH';
+
+module.exports = { FACEBOOK_EMAIL, FACEBOOK_PASSWORD };
